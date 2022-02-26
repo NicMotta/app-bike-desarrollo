0fd4d9df83dc1e9f5f78c2cb02d95ef3e309af66
NicMotta <34142261+NicMotta@users.noreply.github.com>
2022-02-26 09:56:16 -0300

    Primera version - cosas por mejorar

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='font-mono fixed right-0 bottom-0 bg-gray-300 left-0 md:max-w-screen-md m-auto'>
+      <p className='text-xs text-center p-1'>Nic Motta / 2022</p>
+    </footer>
+  )
+}
+
+export default Footer
